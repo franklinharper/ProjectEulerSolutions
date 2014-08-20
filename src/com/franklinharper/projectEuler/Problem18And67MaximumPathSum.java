666fd0c906177301f0ea110cbc5efe33cd5885f3
Frank Harper <frank.harper@gmail.com>
2014-08-20 10:36:26 -0700

    Replace recursive algo. by simpler iterative bottom up algo.

--- src/com/franklinharper/projectEuler/Problem18And67MaximumPathSum.java
+++ src/com/franklinharper/projectEuler/Problem18And67MaximumPathSum.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
-import com.franklinharper.projectEuler.util.ArrayUtils;
 import com.franklinharper.projectEuler.util.IoUtils;
 
 public class Problem18And67MaximumPathSum {
@@ -19,34 +18,33 @@ public class Problem18And67MaximumPathSum {
         maxPathSum( data );
     }
 
-    private static int maxPathSum( int[][] input ) {
+    private static int maxPathSum( int[][] values ) {
+        System.out.println( "Input array" );
+        System.out.println( Arrays.deepToString( values ) );
         long startMillis = System.currentTimeMillis();
-        int[][] resultCache = ArrayUtils.createArray( input );
-        int result = recursiveMaxPathSum( 0, 0, input, resultCache );
+
+        // The trick is to work up from the bottom of the triangular array, rather down from the top.
+        //
+        // On the bottom row, the paths are all of length 1, so the maximum paths are all trivially equal to the input.
+        //
+        //      maximumPath( r, c ) = triangle(r, c)
+        //
+        // For all the preceding rows, the maximumPath(r, c) = value(r, c) + the maximum of the left and right sub trees.
+        //
+        for( int r = values.length - 2; r >= 0; r-- ) {
+            for( int c = 0; c < values[ r ].length; c++ ) {
+                int max = Integer.max( values[ r + 1 ][ c ], values[ r + 1 ][ c + 1 ] );
+                values[ r ][ c ] += max;
+            }
+        }
+
         double elapsedSeconds = ( System.currentTimeMillis() - startMillis ) / 1000.0;
-        System.out.println( "Input data" );
-        System.out.println( Arrays.deepToString( input ) );
-        System.out.println( "Result cache" );
-        System.out.println( Arrays.deepToString( resultCache ) );
+        System.out.println( "Result array" );
+        System.out.println( Arrays.deepToString( values ) );
+        int result = values[ 0 ][ 0 ];
         System.out.println( String.format( "maximumPathSum: %d", result ) );
         System.out.println( String.format( "Elapsed time: %.3fs", elapsedSeconds ) );
         return result;
-    }
-
-    private static int recursiveMaxPathSum( int row, int col, int[][] input, int[][] resultCache ) {
-        if( resultCache[ row ][ col ] != 0 ) {
-            return resultCache[ row ][ col ];
-        }
-        if( row + 1 == input.length ) {
-            // We have hit a leaf node at the bottom the triangle.
-            return input[ row ][ col ];
-        }
-        int maxLeft = recursiveMaxPathSum( row + 1, col, input, resultCache );
-        int maxRight = recursiveMaxPathSum( row + 1, col + 1, input, resultCache );
-        int max = maxLeft > maxRight ? maxLeft : maxRight;
-        max += input[ row ][ col ];
-        resultCache[ row ][ col ] = max;
-        return max;
     }
 
     // JUnit tests below
@@ -54,19 +52,40 @@ public class Problem18And67MaximumPathSum {
     @Test
     public void test() throws Exception {
 
-        assertEquals( 75, maxPathSum( new int[][] { { 75 }, } ) );
+        assertEquals( 75, maxPathSum( new int[][] { 
+            { 75 },
+        } ) );
 
-        assertEquals( 77, maxPathSum( new int[][] { { 75 }, { 1, 2 } } ) );
+        assertEquals( 77, maxPathSum( new int[][] {
+            { 75 }, 
+            { 1, 2 },
+        } ) );
 
-        assertEquals( 77, maxPathSum( new int[][] { { 75 }, { 2, 1 } } ) );
+        assertEquals( 77, maxPathSum( new int[][] {
+            { 75 },
+            { 2, 1 },
+        } ) );
 
-        assertEquals( 15, maxPathSum( new int[][] { { 1 }, { 5, 10 }, { 9, 2, 3 }, } ) );
+        assertEquals( 15, maxPathSum( new int[][] {
+            { 1 },
+            { 5, 10 },
+            { 9, 2, 3 },
+        } ) );
 
-        assertEquals( 20, maxPathSum( new int[][] { { 1 }, { 5, 10 }, { 2, 9, 3 }, } ) );
+        assertEquals( 20, maxPathSum( new int[][] {
+            { 1 },
+            { 5, 10 },
+            { 2, 9, 3 },
+        } ) );
 
-        assertEquals( 22, maxPathSum( new int[][] { { 1 }, { 5, 10 }, { 2, 3, 11 }, } ) );
+        assertEquals( 22, maxPathSum( new int[][] {
+            { 1 },
+            { 5, 10 },
+            { 2, 3, 11 },
+        } ) );
 
         assertEquals( 1074, maxPathSum( IoUtils.createArray( "p018_triangle.txt" ) ) );
+        assertEquals( 7273, maxPathSum( IoUtils.createArray( "p067_triangle.txt" ) ) );
     }
 
 }
